349ca41d8016582fc286a00365ac4525f8869483
박경연 <83110939+gyparkdev@users.noreply.github.com>
2023-10-17 17:29:14 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT board.TITLE, board.BOARD_ID, reply.REPLY_ID, reply.WRITER_ID, reply.CONTENTS, 
+       TO_CHAR(reply.CREATED_DATE, 'YYYY-MM-DD')
+  FROM USED_GOODS_BOARD board 
+ INNER JOIN USED_GOODS_REPLY reply
+    ON board.BOARD_ID = reply.BOARD_ID
+ WHERE TO_CHAR(board.CREATED_DATE, 'YYYY-MM') = '2022-10'
+ ORDER BY reply.CREATED_DATE ASC, board.TITLE ASC;
