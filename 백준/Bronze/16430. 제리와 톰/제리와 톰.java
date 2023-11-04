468b83f22051e43ede217729e99e69fb77e450f1
박경연 <83110939+gyparkdev@users.noreply.github.com>
2023-11-05 00:06:06 +0900

    [Bronze V] Title: 제리와 톰, Time: 236 ms, Memory: 18564 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		int P = sc.nextInt();
+		int Q = sc.nextInt();
+
+		System.out.println(Q-P + " " + Q);
+	}
+}
