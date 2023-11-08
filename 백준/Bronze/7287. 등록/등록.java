bb16608206ace693495a145272b2539b370d65de
박경연 <83110939+gyparkdev@users.noreply.github.com>
2023-11-08 23:16:23 +0900

    [Bronze V] Title: 등록, Time: 116 ms, Memory: 14036 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Main { 
+	
+	public static void main(String[] args) throws IOException { 
+		int N = 45;
+		String ID = "gyparkdev";
+		
+		System.out.println(N);
+		System.out.println(ID);
+	} 
+}
