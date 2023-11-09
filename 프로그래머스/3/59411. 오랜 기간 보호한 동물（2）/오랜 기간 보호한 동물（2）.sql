38bd1b757748456a320c1063c57e980f8471a2b5
박경연 <83110939+gyparkdev@users.noreply.github.com>
2023-11-09 10:28:32 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -0,0 +1,10 @@
+-- 코드를 입력하세요
+SELECT *
+  FROM(
+        SELECT INS.ANIMAL_ID, INS.NAME
+          FROM ANIMAL_INS INS INNER JOIN
+               ANIMAL_OUTS OUTS
+            ON INS.ANIMAL_ID = OUTS.ANIMAL_ID
+         ORDER BY (OUTS.DATETIME - INS.DATETIME) DESC
+       )
+ WHERE ROWNUM <= 2;
