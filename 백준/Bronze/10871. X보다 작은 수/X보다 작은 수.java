63ed82371cf5354bba56f18ed1b139d64b3b69ff
박경연 <83110939+gyparkdev@users.noreply.github.com>
2023-11-12 15:40:19 +0900

    [Bronze V] Title: X보다 작은 수, Time: 592 ms, Memory: 31344 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Main { 
+	
+	public static void main(String[] args) throws IOException { 
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int X = sc.nextInt();
+		int A[] = new int[N];
+		
+		for(int i=0; i<N; i++) {
+			A[i] = sc.nextInt();
+		}
+		
+		for(int i=0; i<A.length; i++) {
+			if(A[i] < X) {
+				System.out.print(A[i] + " ");
+			}
+		}
+	} 
+}
