fcea520fd5636c127b47258c526b3a608f883f66
박경연 <83110939+gyparkdev@users.noreply.github.com>
2023-11-17 10:07:12 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 216 ms, Memory: 18192 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main { 
+
+	public static void main(String[] args) { 
+		Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::" + input + "::fan:");
+        System.out.println(":fan::fan::fan:");
+	} 
+}
