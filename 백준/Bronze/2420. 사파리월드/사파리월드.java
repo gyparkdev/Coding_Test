ddb2d263bad12b33f9541688cc070fbfb66746cb
박경연 <83110939+gyparkdev@users.noreply.github.com>
2023-12-10 09:34:56 +0900

    [Bronze V] Title: 사파리월드, Time: 124 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long N = Integer.parseInt(st.nextToken());
+		long M = Integer.parseInt(st.nextToken());
+		
+		System.out.println(Math.abs(N - M));
+	}
+}
