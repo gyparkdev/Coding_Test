818f29ee76e67f93038f98fff26f45db6ef1bee5
박경연 <83110939+gyparkdev@users.noreply.github.com>
2024-01-03 01:26:50 +0900

    [Bronze IV] Title: 샤틀버스, Time: 216 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/25625. 샤틀버스/샤틀버스.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        if (x > y) {
+            System.out.println(x + y);
+        } else {
+            System.out.println(y - x);
+        }
+
+        // Scanner 사용 후 닫기
+        scanner.close();
+    }
+}
