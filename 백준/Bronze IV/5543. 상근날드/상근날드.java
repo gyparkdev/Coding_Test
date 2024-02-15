f6287b11b806b30e80dba87eb59272dac80cd058
박경연 <83110939+gyparkdev@users.noreply.github.com>
2024-02-15 23:43:24 +0900

    [Bronze IV] Title: 상근날드, Time: 204 ms, Memory: 17540 KB -BaekjoonHub

---
+++ 백준/Bronze IV/5543. 상근날드/상근날드.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String[] args) {
+ 
+		Scanner sc = new Scanner(System.in);
+ 
+		int burger = 2001;
+		int drink = 2001;
+ 
+		// 버거
+		for (int i = 0; i < 3; i++) {
+			int value = sc.nextInt();
+			if (value < burger) {	// 최솟값 찾기
+				burger = value;
+			}
+		}
+ 
+		// 음료
+		for (int i = 0; i < 2; i++) {
+			int value = sc.nextInt();
+			if (value < drink) {	// 최솟값 찾기
+				drink = value;
+			}
+		}
+		System.out.println(burger + drink - 50);
+	}
+ 
+}
