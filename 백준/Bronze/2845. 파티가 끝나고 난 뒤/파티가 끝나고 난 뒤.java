c7f78a42e21d4ef555cb9fe07619cbc83017b3ec
박경연 <83110939+gyparkdev@users.noreply.github.com>
2024-02-29 23:24:43 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 224 ms, Memory: 18256 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+    	Scanner sc = new Scanner(System.in);
+    	int L = sc.nextInt();
+    	int P = sc.nextInt();
+    	
+    	int result = L * P;
+    	
+    	for(int i=0; i<5; i++) {
+    		int N = sc.nextInt();
+    		System.out.print(N - result + " ");
+    	}
+    }
+}
