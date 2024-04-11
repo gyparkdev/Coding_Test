1bbcdfbdfad9841b2ac85082a1037470925edf4a
박경연 <83110939+gyparkdev@users.noreply.github.com>
2024-04-11 15:58:08 +0900

    [Bronze V] Title: SciComLove, Time: 212 ms, Memory: 17656 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main { 
+
+	public static void main(String[] args) throws NumberFormatException, IOException { 
+
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		for(int i=0; i<N; i++) {
+			System.out.println("SciComLove");
+		}
+	}
+}
