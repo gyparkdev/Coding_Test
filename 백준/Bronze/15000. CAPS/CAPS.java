1fc03a09b0274543e0843d3d66d5db469274bef7
박경연 <83110939+gyparkdev@users.noreply.github.com>
2024-04-12 17:55:48 +0900

    [Bronze V] Title: CAPS, Time: 528 ms, Memory: 31052 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main { 
+
+	public static void main(String[] args) throws NumberFormatException, IOException { 
+
+		Scanner sc = new Scanner(System.in);
+		
+		String in = sc.nextLine();
+		String out = in.toUpperCase();
+		
+		System.out.println(out);
+		
+	}
+}
