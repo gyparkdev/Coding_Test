2a711a84f25212ed0ebeead67231a8473db9131e
박경연 <83110939+gyparkdev@users.noreply.github.com>
2024-06-18 11:05:26 +0900

    [level 0] Title: 배열 만들기 4, Time: 5.16 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -2,32 +2,28 @@ import java.util.ArrayList;
 
 class Solution {
     public int[] solution(int[] arr) {
-        ArrayList<Integer> tmp= new ArrayList();
-        
-        int i=0;
-        while(i<arr.length){
-            
-            if(tmp.size()==0){
-                tmp.add(arr[i]);
-                i=i+1;
-            }
-            else{
-                int last=tmp.get(tmp.size()-1);
-                if(tmp.size()!=0 && last<arr[i]){
-                tmp.add(arr[i]);
-                i=i+1;
-                }
-                else if(tmp.size()!=0 && last>=arr[i]){
-                    tmp.remove(tmp.size()-1);
-                }
-            }
-            
-        }
-        
-        int stk[]=new int[tmp.size()];
-        for(int k=0;k<tmp.size();k++){
-            stk[k]=tmp.get(k).intValue();
-        }
+        ArrayList<Integer> tmp = new ArrayList<Integer>();
+		
+		int i = 0;
+		while(i<arr.length) {
+			if(tmp.size() == 0) {
+				tmp.add(arr[i]);
+				i++;
+			}else {
+				int last = tmp.get(tmp.size()-1);
+				if(last < arr[i]) {
+					tmp.add(arr[i]);
+					i++;
+				}else {
+					tmp.remove(tmp.size()-1);
+				}
+			}
+		}
+		
+		int[] stk = new int[tmp.size()];
+		for(int j=0; j<tmp.size(); j++) {
+			stk[j] = tmp.get(j);
+		}
         return stk;
     }
 }
