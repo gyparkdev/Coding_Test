fb4bce85fbe99e4588567153ad664bc4bfca1604
박경연 <83110939+gyparkdev@users.noreply.github.com>
2024-06-27 16:28:49 +0900

    [level 0] Title: 0 떼기, Time: 7.35 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        
+        int str = Integer.parseInt(n_str);
+        answer = str + "";
+        
+        return answer;
+    }
+}
