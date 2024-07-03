2398206a62efe8b1223dac280913bc08f9a837fe
박경연 <83110939+gyparkdev@users.noreply.github.com>
2024-07-03 14:31:55 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 0.03 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
@@ -4,15 +4,15 @@ class Solution {
     public List solution(int[] arr, int[] delete_list) {
         List<Integer> answer = new ArrayList<Integer>();
 		
-		for(int i=0; i<arr.length; i++) {
-			answer.add(arr[i]);
+		for(int i : arr) {
+			answer.add(i);
 		}
 		
-		for(int i=0; i<arr.length; i++) {
-			for(int j=0; j<delete_list.length; j++) {
-				if(arr[i] == delete_list[j]) {
-					answer.remove(Integer.valueOf(arr[i]));
-				}
+		for(int i : arr) {
+			for(int j : delete_list) {
+				if(i == j) {
+					answer.remove(Integer.valueOf(i));
+				}	
 			}
 		}
         
