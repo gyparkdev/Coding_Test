874e9d8ec960842ed100a88e65249973bfbdf655
박경연 <83110939+gyparkdev@users.noreply.github.com>
2024-07-04 14:05:27 +0900

    [level 0] Title: x 사이의 개수, Time: 4.05 ms, Memory: 88.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(String myString) {
+        String[] tmp = {};
+		tmp = myString.split("x", -1);
+		
+		int [] answer = new int[tmp.length];
+		for(int i=0; i<tmp.length; i++) {
+			answer[i] = tmp[i].length();
+		}
+        return answer;
+    }
+}
