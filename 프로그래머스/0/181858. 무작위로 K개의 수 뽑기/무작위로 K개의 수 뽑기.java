b596663ec1d8460cd19b01d52e0b801d0593991d
박경연 <83110939+gyparkdev@users.noreply.github.com>
2024-08-02 17:52:11 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 17.54 ms, Memory: 89.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public List solution(int[] arr, int k) {
+        List<Integer> answer = new ArrayList<>();
+        
+        for(int i = 0; i<arr.length; i++){
+            if(!answer.contains(arr[i])){
+               answer.add(arr[i]);
+            }
+			
+            if(answer.size() == k){
+                break;
+            }
+        }
+		
+        while(answer.size() < k){
+            answer.add(-1);
+        }
+        
+        return answer;
+    }
+}
