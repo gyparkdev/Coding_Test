92d155ce8d5d1f97801bf20a1570ac58f0395997
박경연 <83110939+gyparkdev@users.noreply.github.com>
2024-09-09 00:10:47 +0900

    [level 0] Title: 가위 바위 보, Time: 2.65 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -2,12 +2,11 @@ class Solution {
     public String solution(String rsp) {
         String answer = "";
         
-        String[] arr = rsp.split("");
-    	
-    	for(int i=0; i<arr.length; i++) {
-    		if(arr[i].equals("2")) {
+        //가위 2, 바위 0, 보 5
+        for(int i=0; i<rsp.length(); i++) {
+    		if(rsp.charAt(i) == '2') {
     			answer += "0";
-    		}else if(arr[i].equals("0")) {
+    		}else if(rsp.charAt(i) == '0') {
     			answer += "5";
     		}else {
     			answer += "2";
